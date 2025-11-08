057da43aa9f9fe580c9ac3a50a55e5b9705288cf
Nathan Andrade <nathan.nhdsk@gmail.com>
2025-11-08 00:01:37 -0300

    chore: create folder for must-do list

--- 27.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-def remove_element(nums: List[int], val: int) -> int: 
-    for num_idx in range(len(nums)):
-        if nums[num_idx] == val:
-            nums[num_idx] = "_"
-    
-    nums[:] = [num for num in nums if num != "_"]
-    return len(nums)
